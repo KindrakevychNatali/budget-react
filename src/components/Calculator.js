9e9e675a4a93639696608f9db915108ceae478af
Nataliia_Kindrakevych <115713835+KindrakevychNatali@users.noreply.github.com>
2024-07-25 13:21:41 +0100

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-
+// components/Calculator.js
 import React, { useState } from 'react';
 import './Calculator.css';
 
@@ -35,15 +35,15 @@ const Calculator = ({ onClose }) => {
         <div className="keypad">
           <button className="highlight" onClick={clear} id="clear">Clear</button>
           <button className="highlight" onClick={backspace} id="backspace">C</button>
-          <button className="highlight" name="/" onClick={handleClick}>/</button>
+          <button className="highlight" name="/" onClick={handleClick}>&divide;</button>
           <button name="7" onClick={handleClick}>7</button>
           <button name="8" onClick={handleClick}>8</button>
           <button name="9" onClick={handleClick}>9</button>
-          <button className="highlight" name="*" onClick={handleClick}>*</button>
+          <button className="highlight" name="*" onClick={handleClick}>&times;</button>
           <button name="4" onClick={handleClick}>4</button>
           <button name="5" onClick={handleClick}>5</button>
           <button name="6" onClick={handleClick}>6</button>
-          <button className="highlight" name="-" onClick={handleClick}>-</button>
+          <button className="highlight" name="-" onClick={handleClick}>&ndash;</button>
           <button name="1" onClick={handleClick}>1</button>
           <button name="2" onClick={handleClick}>2</button>
           <button name="3" onClick={handleClick}>3</button>
@@ -58,4 +58,3 @@ const Calculator = ({ onClose }) => {
 };
 
 export default Calculator;
-
