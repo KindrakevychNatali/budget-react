b98e16cad0bc82906c786abe44a440efe2fc4bf7
Nataliia_Kindrakevych <115713835+KindrakevychNatali@users.noreply.github.com>
2024-07-25 13:11:13 +0100

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,54 +2,60 @@
 import React, { useState } from 'react';
 import './Calculator.css';
 
-const Calculator = () => {
-    const [input, setInput] = useState('');
-    const [result, setResult] = useState('');
+const Calculator = ({ onClose }) => {
+  const [result, setResult] = useState('');
 
-    const handleClick = (value) => {
-        setInput(input + value);
-    };
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
 
-    const handleClear = () => {
-        setInput('');
-        setResult('');
-    };
+  const clear = () => {
+    setResult('');
+  };
 
-    const handleCalculate = () => {
-        try {
-            const evalResult = eval(input); // Be cautious using eval
-            setResult(evalResult);
-        } catch (error) {
-            setResult('Error');
-        }
-    };
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  };
 
-    return (
-        <div className="calculator">
-            <div className="display">
-                <div>{input}</div>
-                <div>{result}</div>
-            </div>
-            <div className="buttons">
-                <button onClick={() => handleClick('1')}>1</button>
-                <button onClick={() => handleClick('2')}>2</button>
-                <button onClick={() => handleClick('3')}>3</button>
-                <button onClick={() => handleClick('+')}>+</button>
-                <button onClick={() => handleClick('4')}>4</button>
-                <button onClick={() => handleClick('5')}>5</button>
-                <button onClick={() => handleClick('6')}>6</button>
-                <button onClick={() => handleClick('-')}>-</button>
-                <button onClick={() => handleClick('7')}>7</button>
-                <button onClick={() => handleClick('8')}>8</button>
-                <button onClick={() => handleClick('9')}>9</button>
-                <button onClick={() => handleClick('*')}>*</button>
-                <button onClick={handleClear}>C</button>
-                <button onClick={() => handleClick('0')}>0</button>
-                <button onClick={handleCalculate}>=</button>
-                <button onClick={() => handleClick('/')}>/</button>
-            </div>
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  return (
+    <div className="calculator-modal">
+      <div className="calculator-content">
+        <span className="close-button" onClick={onClose}>&times;</span>
+        <form>
+          <input type="text" value={result} readOnly />
+        </form>
+        <div className="keypad">
+          <button className="highlight" onClick={clear} id="clear">Clear</button>
+          <button className="highlight" onClick={backspace} id="backspace">C</button>
+          <button className="highlight" name="/" onClick={handleClick}>/</button>
+          <button name="7" onClick={handleClick}>7</button>
+          <button name="8" onClick={handleClick}>8</button>
+          <button name="9" onClick={handleClick}>9</button>
+          <button className="highlight" name="*" onClick={handleClick}>*</button>
+          <button name="4" onClick={handleClick}>4</button>
+          <button name="5" onClick={handleClick}>5</button>
+          <button name="6" onClick={handleClick}>6</button>
+          <button className="highlight" name="-" onClick={handleClick}>-</button>
+          <button name="1" onClick={handleClick}>1</button>
+          <button name="2" onClick={handleClick}>2</button>
+          <button name="3" onClick={handleClick}>3</button>
+          <button className="highlight" name="+" onClick={handleClick}>+</button>
+          <button name="0" onClick={handleClick}>0</button>
+          <button name="." onClick={handleClick}>.</button>
+          <button className="highlight" onClick={calculate} id="result">=</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Calculator;
+
